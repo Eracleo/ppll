40c692377eba616ec42614700bd291cdf3bf6695
Juan <114143@unsaac.edu.pe>
2015-02-20 11:13:57 -0500

    he agregado la clase Persona, y ignorar los archivos compilados

--- pyllik/models.py
+++ pyllik/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=60)
+    doc_tipo = models.CharField(max_length=10)
+    doc_nro = models.CharField(max_length=10)
+    def __unicode__(self):
+        return self.nombre
