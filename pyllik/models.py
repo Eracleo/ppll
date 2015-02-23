42e4f44c0f3d4d00fdad4b9a2fe91c5ae4045261
Juan <114143@unsaac.edu.pe>
2015-02-23 12:38:45 -0500

    En el modeo he agregado fecha actual por defecto, ignore urls.pyc

--- pyllik/models.py
+++ pyllik/models.py
@@ -5,31 +5,35 @@ class Pais(models.Model):
     nombre = models.CharField(max_length=30)
     def __unicode__(self):
         return self.nombre
+DOC_TIPO = (
+    ('dni','DNI'),
+    ('pas','Passport'),
+    )
 class Persona(models.Model):
     nombre = models.CharField(max_length=30)
     apellidos = models.CharField(max_length=60)
-    doc_tipo = models.CharField(max_length=10)
+    doc_tipo = models.CharField(max_length=3, choices=DOC_TIPO)
     doc_nro = models.CharField(max_length=10)
     pais = models.ForeignKey(Pais)
-    edad = models.IntegerField()
-    creado = models.DateField()
+    # edad = models.IntegerField()
+    creado = models.DateField(auto_now_add=True)
     def __unicode__(self):
         return self.nombre
 class Paquete(models.Model):
     nombre = models.CharField(max_length=20)
-    precio = models.FloatField()
-    descripcion = models.TextField()
+    precio = models.FloatField(default=0)
+    descripcion = models.TextField(blank=True)
     user = models.ForeignKey(User)
-    creado = models.DateField()
+    creado = models.DateField(auto_now_add=True)
     estado = models.BooleanField(default=True)
     def __unicode__(self):
         return self.nombre
 class Reserva(models.Model):
     paquete = models.ForeignKey(Paquete)
     cantidad_personas = models.IntegerField(default=0)
-    precio = models.FloatField()
+    precio = models.FloatField(default=0)
     user = models.ForeignKey(User)
-    creado = models.DateTimeField()
+    creado = models.DateTimeField(auto_now_add=True)
     estado = models.BooleanField(default=True)
     def save(self, *args, **kwargs):
         self.user = self.paquete.user
