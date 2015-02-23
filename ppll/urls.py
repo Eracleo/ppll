7ce2636f55ee5d66789af02a116b20481f600531
Juan <114143@unsaac.edu.pe>
2015-02-23 12:18:33 -0500

    ignorar el archivo compilado urls.pyc

--- ppll/urls.py
+++ ppll/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from pyllik import views
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$',views.index,name='index'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
