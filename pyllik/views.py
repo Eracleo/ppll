8b0036ca298d9f662440608b16d371f862be1af5
Luis2309 <luisjfr2309@gmail.com>
2015-02-25 10:37:55 -0500

    Update views.py

--- pyllik/views.py
+++ pyllik/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Paqu
+from .models import Paquete
 
 def index(request):
     return render(request,'index.html')
