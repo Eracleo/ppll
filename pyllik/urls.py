79b4a7297bce114e05328bc9e625a817e7471200
Juan <114143@unsaac.edu.pe>
2015-02-27 23:53:19 -0500

    He agregado urls para el crud de paquete, empresa, reserva

--- pyllik/urls.py
+++ pyllik/urls.py
@@ -3,4 +3,17 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$','pyllik.views.index', name='index'),
     url(r'^paquete.html$','pyllik.views.detalle', name='detalle'),
+    # Paquetes
+    url(r'^paquetes$','pyllik.views.paqueteListar', name='paqueteListar'),
+    url(r'^paquete/detalle/(?P<id>\d+)$','pyllik.views.paqueteDetalle', name='paqueteDetalle'),
+    url(r'^paquete/edit/(?P<id>\d+)$','pyllik.views.paqueteEdit', name='paqueteEdit'),
+    url(r'^paquete/add$','pyllik.views.paqueteAdd', name='paqueteAdd'),
+    # Agencia
+    url(r'^agencia$','pyllik.views.agenciaDetalle', name='agenciaDetalle'),
+    url(r'^agencia/edit/(?P<id>\d+)$','pyllik.views.agenciaEdit', name='agenciaEdit'),
+    # Reservas
+    url(r'^reservas$','pyllik.views.reservaListar', name='reservaListar'),
+    url(r'^reserva/detalle/(?P<id>\d+)$','pyllik.views.reservaDetalle', name='reservaDetalle'),
+    url(r'^reserva/edit/(?P<id>\d+)$','pyllik.views.reservaEdit', name='reservaEdit'),
+    url(r'^reserva/add$','pyllik.views.reservaAdd', name='reservaAdd'),
 )
