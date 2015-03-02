26409ea8dcf2fcc22e227cf649e7ea3201568b78
einel <einel@llika.net>
2015-03-02 10:16:05 -0500

    Hemos agregado la direccion a detalle de reserva para el formulario persona

--- reservar/urls.py
+++ reservar/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^$','reservar.views.index', name='index'),
     url(r'^reservar$','reservar.views.index', name='index'),
     url(r'^paquete.html$','reservar.views.detalle', name='detalle'),
+    url(r'^dato.html$','reservar.views.persona', name='persona'),
 )
