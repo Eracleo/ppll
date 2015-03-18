2df0b310959eb90ec9ec753206f68cf02296c8cd
aridni81 <aridni81@gmail.com>
2015-03-18 13:10:29 -0500

    Hemos terminado la reserva y el ingreso de datos de los viajeros

--- reservar/forms.py
+++ reservar/forms.py
@@ -1,24 +1,18 @@
 from django import forms
 from pyllik.models import Reserva, Persona
+
 from django.forms.formsets import formset_factory
 
 class PostForm(forms.Form):
     Paquete_Id = forms.CharField(max_length=256)
     Cantidad_Personas = forms.CharField(max_length=256)
     Fecha = forms.DateTimeField()
+    
 class ReservarForm(forms.ModelForm):
     class Meta:
         model = Reserva
+
 class ContactoForm(forms.Form):
 	correo = forms.EmailField()
 	mensaje = forms.CharField(widget=forms.Textarea)
-	
-class PersonaForm(forms.ModelForm):
-    class Meta:
-        model = Persona
 
-PersonaFormset= formset_factory(PersonaForm, extra=2, max_num=3)
-
-class ReservaaForm(forms.Form):
-    paquete= forms.CharField(max_length=100)    
-    viajeros= PersonaFormset()
--- reservar/views.py
+++ reservar/views.py
@@ -3,9 +3,9 @@ from .forms import PostForm
 from django.views.generic import ListView
 from pyllik.models import Paquete, Pais, Reserva,Persona
 from reservar.forms import PostForm
-from reservar.forms import PersonaForm
-from reservar.forms import PersonaFormset
-from reservar.forms import ReservaaForm
+#from reservar.forms import PersonaForm
+#from reservar.forms import PersonaFormset
+#from reservar.forms import ReservaaForm
 from reservar.forms import PostForm, ContactoForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
@@ -17,6 +17,9 @@ from  django.forms.models  import  inlineformset_factory
 from django.template import RequestContext
 from django.forms.formsets import formset_factory
 from django.core.mail import EmailMessage
+from django import forms
+from pyllik.models import Reserva, Persona
+
  
 
 def index(request):
@@ -57,22 +60,98 @@ def detalle(request, id):
         return render(request,'detalle.html',context)
     else :
         HttpResponseRedirect('/')
-# Creando formulario registro personas con comboBox pais-gfgd
-# Creando formulario registro personas con comboBox pais-gfgd
-def personasa(request):
-    paquete_id =  request.POST.get('paquete_id')
-    paquete = Paquete.objects.get(id=paquete_id)
-    cantidad_personas = request.POST.get('cantidad')
-    fecha_viaje = request.POST.get('fecha')
-    monto = request.POST.get('id_monto')
-    if request.method == 'POST':        
+
+
+def pasajeros(request,id):
+    reserva = Reserva.objects.get(id=id)
+    return render(request,'pasajeros.html',{'reserva':reserva})
+
+def Post(request):    
         form = ReservaaForm(request.POST)
         form.viajeros_instances = PersonaFormset(request.POST)
         if form.is_valid():
             #reserva = Reserva() #model class
             #reserva.paquete= form.cleaned_data() 
+            paquete=Paquete.objects.get(id=1)
+            reserva = Reserva(paquete=paquete, cantidad_personas="2", fecha_viaje="2014-04-23", precio="1200")
+            reserva.save()
+            if form.viajeros_instances.cleaned_data is not None:
+                for item in form.viajeros_instances.cleaned_data:
+                    persona = Persona() #Product model class
+                    persona.nombre= item['nombre']
+                    persona.apellidos= item['apellidos']
+                    persona.doc_tipo= item['doc_tipo']
+                    persona.doc_nro= item['doc_nro']
+                    persona.pais= item['pais']
+                    persona.email= item['email']
+                    persona.save()
+                    reserva.viajeros.add(persona)
+
+            return HttpResponseRedirect('/reservar/success')
+        return HttpResponseRedirect('/reservar/failure')
+
+def Failure(request):
+    form = ReservaaForm()
+    form.persona_instances = PersonaFormset()
+    return render(request,'persona.html',{'form':form})
+def Success(request):
+    reserva = Reserva.objects.all()
+    return render(request,'lista.html',{'reserva':reserva})
+def Detalle(request,id):
+    reserva = Reserva.objects.get(id=id)
+    viajeros = reserva.viajeros.all()
+    return render(request,'detalle1.html',{'Reserva':reserva, 'viajeros':viajeros})
+
+#Correo de Confirmacion
+def confircorreo(request):
+    if request.method=='POST':
+        formulario = ContactoForm(request.POST)
+        if formulario.is_valid():
+            titulo = 'Mensaje des pagos en LLIKA EIRL'
+            contenido = formulario.cleaned_data['mensaje'] + "\n"
+            contenido += 'Comunicarse a: ' + formulario.cleaned_data['correo']
+            correo = EmailMessage(titulo, contenido, to=['aridni81@gmail.com'])
+            correo.send()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = ContactoForm()
+    return render_to_response('confirmar.html',{'formulario':formulario}, context_instance=RequestContext(request))
+
+
+
+def personasa(request):
+    # Creando formulario registro personas con comboBox pais-gfgd
+    paquete_id =  request.POST.get('paquete_id')
+    paquete = Paquete.objects.get(id=paquete_id)
+    cantidad_personas = request.POST.get('cantidad')
+    fecha_viaje = request.POST.get('fecha')
+    monto = request.POST.get('id_monto')
+
+    class PersonaForm(forms.ModelForm):
+        class Meta:
+            model = Persona
+
+    #PersonaFormset= formset_factory(PersonaForm,extra=2,max_num=3)
+    PersonaFormset= formset_factory(PersonaForm, extra=int(cantidad_personas), max_num=int(cantidad_personas))
+
+    class ReservaaForm(forms.Form):
+
+        paquete= forms.CharField(max_length=100)    
+        viajeros= PersonaFormset()
+
+    
+    if request.method == 'POST':        
+        form = ReservaaForm(request.POST)
+        #user_id = request.user.id
+        
+        form.viajeros_instances = PersonaFormset(request.POST)
+        if form.is_valid():
+            #reserva = Reserva() #model class
+            #reserva.paquete= form.cleaned_data() 
             paquete=Paquete.objects.get(id=paquete_id)
-            reserva = Reserva(paquete=paquete, cantidad_personas=cantidad_personas, fecha_viaje=fecha_viaje, precio=monto)
+
+            reserva = Reserva(paquete=paquete, cantidad_personas=cantidad_personas, fecha_viaje=fecha_viaje, precio=monto,user=request.user)
+            #reserva.user_instances=user_id
             reserva.save()
             if form.viajeros_instances.cleaned_data is not None:
                 for item in form.viajeros_instances.cleaned_data:
@@ -112,37 +191,4 @@ def personasa(request):
             'monto':monto,
             'form':form          
         }
-        return render(request,'jajaj.html',context)   
-        
-def Success(request):
-    reserva = Reserva.objects.all()
-    return render(request,'lista.html',{'reserva':reserva})
-def Detalle(request,id):
-    reserva = Reserva.objects.get(id=id)
-    viajeros = reserva.viajeros.all()
-    return render(request,'detalle1.html',{'Reserva':reserva, 'viajeros':viajeros})
-def Failure(request):
-    form = ReservaaForm()
-    form.persona_instances = PersonaFormset()
-    return render(request,'persona.html',{'form':form})
-    
-
-
-def pasajeros(request,id):
-    reserva = Reserva.objects.get(id=id)
-    return render(request,'pasajeros.html',{'reserva':reserva})
-
-#Correo de Confirmacion
-def confircorreo(request):
-    if request.method=='POST':
-        formulario = ContactoForm(request.POST)
-        if formulario.is_valid():
-            titulo = 'Mensaje des pagos en LLIKA EIRL'
-            contenido = formulario.cleaned_data['mensaje'] + "\n"
-            contenido += 'Comunicarse a: ' + formulario.cleaned_data['correo']
-            correo = EmailMessage(titulo, contenido, to=['aridni81@gmail.com'])
-            correo.send()
-            return HttpResponseRedirect('/')
-    else:
-        formulario = ContactoForm()
-    return render_to_response('confirmar.html',{'formulario':formulario}, context_instance=RequestContext(request))
+        return render(request,'jajaj.html',context)
