3ae52ae836a6d7d3905deabeb79a11198f0e7fa2
Luis2309 <luisjfr2309@gmail.com>
2015-03-31 12:54:58 -0500

    SKU como sugerencia y siempre en letras mayusculas

--- ppll/settings.py
+++ ppll/settings.py
@@ -99,7 +99,7 @@ LOGIN_URL = '/user/login'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'luisjfr2309@gmail.com'
-EMAIL_HOST_PASSWORD = '101030luisito'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
