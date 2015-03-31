0434e00bdb2def28a3459abe1c6f8b50ba424c60
aridni81 <aridni81@gmail.com>
2015-03-31 10:41:19 -0500

    He modificado el cambio de contraseña

--- usuario/views.py
+++ usuario/views.py
@@ -52,3 +52,7 @@ def config(request):
         return HttpResponseRedirect('/user')
     except Empresa.DoesNotExist:
         return HttpResponseRedirect('/empresa/information')
+
+@login_required()
+def cambiar(request):
+    return render(request,'cambiarcontra.html', {'user': request.user})
