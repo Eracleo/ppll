4da9d42382e3196938ce311a8f7ffd9d409f34f3
JuanEracleo <juan@llika.net>
2015-03-31 18:28:47 -0500

    He arreglado estiglos, agregado contador a personas.html, algunos campos text he cambiado a hidden

--- reservar/paypal.py
+++ reservar/paypal.py
@@ -1,12 +1,12 @@
 import urllib
-import urllib2 
+import urllib2
 def paypal_check(tx,at):
     success = False
     post_data = [('cmd','_notify-synch'),('tx',tx),('at',at)]     # a sequence of two element tuples
     result = urllib2.urlopen('https://www.sandbox.paypal.com/au/cgi-bin/webscr', urllib.urlencode(post_data))
     content = result.read().split('\n')
     results = {}
- 
+
     for each in content[1:]:
         print each
         try:
@@ -17,5 +17,5 @@ def paypal_check(tx,at):
             pass
     if content[0].find('SUCCESS') >= 0:
         success = True
- 
+
     return success, results
