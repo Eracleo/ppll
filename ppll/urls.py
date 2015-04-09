1d46de9124b77edc25023cacf1f120ea1e2afdbd
JuanEracleo <juan@llika.net>
2015-04-09 10:48:01 -0500

    He agregado la configuracion y templates de errores 404 403 500

--- ppll/urls.py
+++ ppll/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from page import views
 from django.conf import settings
 
+handler404 = 'pyllik.views.error404'
+handler403 = 'pyllik.views.error403'
+handler500 = 'pyllik.views.error500'
+
 urlpatterns = patterns('',
     url(r'^$',views.index,name='index'),
     url(r'^admin/', include(admin.site.urls)),
