bfdbad5a9d0b38ffec39961853e80c4e8fa02413
JuanEracleo <juan@llika.net>
2015-04-17 17:57:00 -0500

    he agregado validaciones al formulario de registro

--- usuario/forms.py
+++ usuario/forms.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 class SignUpForm(ModelForm):
-	class Meta:
+    username = forms.CharField(required=True,min_length=5,max_length=15)
+    first_name = forms.CharField(required=True,min_length=3,max_length=25, label="Nombre(s)")
+    last_name = forms.CharField(required=True,max_length=45,label="Apellidos")
+    email = forms.EmailField(required=True,max_length=60)
+    class Meta:
 		model = User
 		fields = ['username', 'password', 'email', 'first_name', 'last_name']
 		widgets = {
