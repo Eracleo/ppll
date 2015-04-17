8395805501de0369f82b455c8ce0f8c07190d8c9
JuanEracleo <juan@llika.net>
2015-04-17 17:56:27 -0500

    he agregado validaciones

--- pyllik/forms.py
+++ pyllik/forms.py
@@ -1,18 +1,34 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from .models import Paquete, Empresa
+from suit_ckeditor.widgets import CKEditorWidget
 class PaqueteForm(forms.ModelForm):
+    descripcion = forms.Textarea()
+    precio = forms.FloatField(label="Precio($)")
+    estado = forms.RadioSelect()
     class Meta:
         model = Paquete
         fields = ('sku','nombre', 'precio','porcentaje','pre_pago','descripcion','link','estado')
         widgets = {
-            'sku': forms.HiddenInput()
+            'sku': forms.HiddenInput(),
+            'descripcion': CKEditorWidget(editor_options={'startupFocus': True})
         }
 class EmpresaForm(forms.ModelForm):
+    razon_social = forms.CharField(max_length=40)
+    direccion = forms.CharField(max_length=40)
+    telefono = forms.CharField(max_length=30)
+    ruc = forms.CharField(min_length=11)
+    abreviatura = forms.CharField(min_length=3)
     class Meta:
         model = Empresa
-        exclude = ('user','nro_paquetes')
+        exclude = ('user','nro_paquetes','paypal_email','paypal_at')
 
 class EmpresaFormEdit(forms.ModelForm):
+    razon_social = forms.CharField(max_length=40)
+    direccion = forms.CharField(max_length=40)
+    telefono = forms.CharField(max_length=30)
+    ruc = forms.CharField(min_length=11)
+    abreviatura = forms.CharField(min_length=3)
     class Meta:
         model = Empresa
         exclude = ('user','nro_paquetes','logo','abreviatura')
