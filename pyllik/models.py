469359e05fed307ffc17cb73f724763370fe2cdb
JuanEracleo <juan@llika.net>
2015-05-11 15:38:23 -0500

    Generar sku de forma automatica al momento de guardarse en paquete, y abreviatura para empresa

--- pyllik/models.py
+++ pyllik/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from thumbs import ImageWithThumbsField
+import uuid,random
+def Abreviatura(razon_social):
+    abreviatura = razon_social[:3]
+    abreviatura += random.choice("abcdefghijklm")
+    return abreviatura.upper()
 class Pais(models.Model):
     nombre = models.CharField(max_length=30)
     def __unicode__(self):
@@ -12,7 +17,7 @@ class Rubro(models.Model):
     def __unicode__(self):
         return self.nombre
 class Empresa(models.Model):
-    rubro = models.ForeignKey(Rubro)
+    rubro = models.ForeignKey(Rubro,default=1)
     razon_social = models.CharField(max_length=100)
     ruc = models.CharField(max_length=11)
     direccion = models.CharField(max_length=120)
@@ -23,13 +28,16 @@ class Empresa(models.Model):
     web = models.URLField(max_length=64, blank=True)
     logo = ImageWithThumbsField(upload_to='logos_empresa', sizes=((150,50),))
     terminos_condiciones = models.URLField(max_length=120, blank=True, help_text="URL Terminos y condiciones.")
-    abreviatura = models.CharField(max_length=3,unique=True)
+    abreviatura = models.CharField(max_length=4,unique=True,editable=False)
     paypal_email = models.EmailField(max_length=100,help_text="E-mail relacionado con paypal")
     paypal_at = models.CharField(max_length=64,help_text="Código de identicacion en paypal") # IdentityToken
-    code = models.CharField(max_length=32,blank=True,editable=False)
-    trabajadores = models.ManyToManyField(User)
+    code = models.CharField(max_length=32,default=uuid.uuid1().hex,editable=False)
     creado = models.DateField(auto_now_add=True, editable=False)
     editado = models.DateTimeField(auto_now=True, editable=False)
+    def save(self, *args, **kwargs):
+        if len(self.abreviatura) < 2:
+            self.abreviatura = Abreviatura(self.razon_social)
+        super(Empresa,self).save(*args,**kwargs)
     def __unicode__(self):
         return self.razon_social
 class TipoDocumento(models.Model):
@@ -37,9 +45,8 @@ class TipoDocumento(models.Model):
     def __unicode__(self):
         return self.nombre
 class Trabajador(models.Model):
-    user = models.ForeignKey(User)
-    doc_tipo = models.ForeignKey(TipoDocumento)
-    doc_nro = models.CharField(max_length=10)
+    user = models.OneToOneField(User,primary_key=True)
+    doc_nro = models.CharField(max_length=10,verbose_name="D. I.")
     direccion = models.CharField(max_length=60)
     email = models.CharField(max_length=60)
     telefono = models.CharField(max_length=60)
@@ -48,7 +55,7 @@ class Trabajador(models.Model):
     empresa = models.ForeignKey(Empresa)
     foto = ImageWithThumbsField(upload_to='logos_empresa', sizes=((150,250),))
     def __unicode__(self):
-        return self.user
+        return str(self.user)
 class TrabajadorContacto(models.Model):
     nombre = models.CharField(max_length=30)
     apellidos = models.CharField(max_length=50)
@@ -58,7 +65,7 @@ class TrabajadorContacto(models.Model):
     trabajador = models.ForeignKey(Trabajador)
     empresa = models.ForeignKey(Empresa)
     def __unicode__(self):
-        return self.apellidos+" " self.nombre
+        return self.nombre
 class TipoPasajero(models.Model):
     nombre = models.CharField(max_length=30)
     def __unicode__(self):
@@ -79,8 +86,12 @@ class Pasajero(models.Model):
     def __unicode__(self):
         return self.nombre
 BOOL_CHOICES = ((True, 'Habitilido'), (False, 'Deshabilitado'))
+def sku(last_sku):
+    sku = last_sku[:4]
+    sku += str(int(last_sku[4:]) + 1).zfill(3)
+    return sku;
 class Paquete(models.Model):
-    sku = models.CharField(max_length=6,unique=True)
+    sku = models.CharField(max_length=7,unique=True,editable=False)
     nombre = models.CharField(max_length=60,help_text="Coloque el nombre del paquete, tal como aparece en su sitio web.")
     precio = models.FloatField(default=0, validators=[MinValueValidator(0)],help_text="Coloque el precio del paquete, tal como aparece en su sitio web. Los precios son en Dólares Americanos (USD $)")
     porcentaje = models.FloatField(default=100, validators=[MinValueValidator(0),MaxValueValidator(100)],help_text="Coloque el porcentaje del Pre pago que el sistema va a cobrar por pasajero, el monto en USD $ se actualiza automáticamente.")
@@ -91,6 +102,12 @@ class Paquete(models.Model):
     estado = models.BooleanField(default=True,choices=BOOL_CHOICES, help_text="Active el Estado si desea que este paquete este activo, si quita el check el paquete se desactivará y no podrá userse.")
     creado = models.DateField(auto_now_add=True, editable=False)
     editado = models.DateTimeField(auto_now=True, editable=False)
+    def save(self, *args, **kwargs):
+        if len(self.sku) < 7:
+            empresa_id = self.empresa.id
+            ultimo = Paquete.objects.filter(empresa_id = empresa_id).latest('id')
+            self.sku = sku(ultimo.sku)
+        super(Paquete,self).save(*args,**kwargs)
     def __unicode__(self):
         return self.nombre
 class TipoCliente(models.Model):
@@ -142,12 +159,13 @@ class Reserva(models.Model):
     cliente = models.ForeignKey(Cliente)
     estado = models.ForeignKey(EstadoReserva,verbose_name="Estado Reserva")
     estado_pago = models.ForeignKey(EstadoPago)
-    code = models.CharField(max_length=32,editable=False)
+    code = models.CharField(max_length=32,default=uuid.uuid1().hex,editable=False)
     nuevo = models.BooleanField(default=False)
     creado = models.DateTimeField(auto_now_add=True, editable=False)
     editado = models.DateTimeField(auto_now=True, editable=False)
     empresa = models.ForeignKey(Empresa)
     def save(self, *args, **kwargs):
+    # Esto esta mal
         self.empresa = self.paquete.empresa
         self.pre_pago = self.paquete.pre_pago
         self.precio = self.paquete.precio
@@ -172,7 +190,7 @@ class Pago(models.Model):
     precio = models.FloatField(default=0,verbose_name="Monto pagado")
     creado = models.DateTimeField(null=True,verbose_name="Fecha de pago")
     forma_pago = models.ForeignKey(FormaPago)
-    estado = models.BooleanField(default=True,choices=BOOL_CHOICES)
+    estado = models.BooleanField(default=True,choices=PAGO_CHOICES)
     ip = models.GenericIPAddressField(null=True,blank=True)
     tx = models.CharField(max_length=64, blank=True)
     verificado = models.BooleanField(default=False)
